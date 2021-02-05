8cbc5174c2bbce32fc0e627da4be66f7ee72440d
adrdantas <78499895+adrdantas@users.noreply.github.com>
2021-02-05 14:55:47 -0300

    Update settings.py

--- ProyectWeb/settings.py
+++ ProyectWeb/settings.py
@@ -135,5 +135,5 @@ EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = 'smtp.office365.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "dryka_spo@hotmail.com"
+EMAIL_HOST_USER = " "
 EMAIL_HOST_PASSWORD = ''
